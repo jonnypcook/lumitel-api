0b37cf61a7790db7513b9cdb5c649afda9664c67
jonnypcook <jonny.p.cook@8point3led.co.uk>
2017-02-23 16:48:43 +0000

    Lightwave config and data structure

--- config/iot.php
+++ config/iot.php
@@ -17,6 +17,17 @@ return [
             'device' => 'http://portal.liteip.com/8p3/GetDevice.aspx?DrawingID=%d&E3Only=%d',
         ]
     ],
-    'litewaverf' => ['*'],
+    'litewaverf' => [
+        'credentials' => [
+            'secret' => 'cab2c73132e764003cb3',
+            'clientId' => 'e629f50f-d87a-4e02-8f01-f5cc7127d241'
+        ],
+        'api' => [
+            'discovery' => 'https://pub-api.lightwaverf.com/api/com/discovery',
+            'command' => 'https://pub-api.lightwaverf.com/api/com/device/%d/command/%s/%s',
+            'auth' => 'https://auth.lightwaverf.com/token',
+            'device-data' => 'https://pub-api.lightwaverf.com/api/com/device/%d/data/%s?from=%s&to=%s&perpage=%d&page=%d',
+        ]
+    ],
 ];
 
--- database/migrations/2016_12_12_174539_create_lightwaves_table.php
+++ database/migrations/2016_12_12_174539_create_lightwaves_table.php
@@ -15,8 +15,30 @@ class CreateLightwavesTable extends Migration
     {
         Schema::create('device_lightwave', function (Blueprint $table) {
             $table->increments('device_lightwave_id');
-            $table->string('code');
-            $table->unsignedInteger('vendor_id');
+            $table->unsignedInteger('vendor_id'); // equivalent to device_id
+
+            // lightWave identifiers
+            $table->unsignedInteger('room_id')->default(0);
+            $table->unsignedInteger('zone_id')->default(0);
+            $table->unsignedInteger('user_id')->default(0);
+            $table->unsignedInteger('device_id');
+            $table->unsignedInteger('device_type_id');
+            $table->unsignedInteger('wfl_id')->default(0);
+            $table->unsignedInteger('dm_id')->default(0);
+
+            // other
+            $table->string('name', 255);
+            $table->boolean('active');
+            $table->tinyInteger('device_number');
+            $table->string('serial', 64);
+            $table->integer('rank')->nullable();
+            $table->integer('energy_rank')->nullable();
+            $table->integer('trigger_rank')->nullable();
+            $table->integer('heating_rank')->nullable();
+            $table->float('unit_rate', 8, 2)->nullable()->default(0);
+            $table->string('device_type_name', 255)->nullable();
+            $table->string('wfl_code', 3)->nullable();
+            $table->boolean('is_heating')->default(false);
             $table->timestamps();
         });
     }
